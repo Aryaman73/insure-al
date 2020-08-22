04925ad2625de779c08bb06f4c026ac75eeb2afe
Aryaman73 <aryaman.singh.2012@gmail.com>
2020-08-22 19:48:36 -0400

    Updates

---
+++ precache-manifest.ad3a47e061d1230a2af7422e21d89815.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a8207820859ef83e661b5f1c4a6b27e",
+    "url": "/insure-al/index.html"
+  },
+  {
+    "revision": "7d5e5d99232bb7f515f1",
+    "url": "/insure-al/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5e8251ecd344ed3d7807",
+    "url": "/insure-al/static/js/2.49689844.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/insure-al/static/js/2.49689844.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d5e5d99232bb7f515f1",
+    "url": "/insure-al/static/js/main.d1cd1416.chunk.js"
+  },
+  {
+    "revision": "5d8176facf443914fccb",
+    "url": "/insure-al/static/js/runtime-main.21c5ab1a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/insure-al/static/media/logo.5d5d9eef.svg"
+  }
+]);
